56189192fd5e67d9d2abe7ff61b4cdf62eb26bd1
Lucas Sabathé <luqui31@gmail.com>
2021-01-14 17:48:11 +0100

    index-logo

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CMB_Photographie</title>
-    <link rel="icon" type="image/png" href="includes/logo.png" />
+    <link rel="icon" type="image/png" href="includes/favicon.png" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="app/css/style.css">
     <link rel="stylesheet" href="app/css/index.css">
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CMB_Photographie</title>
-</head>
-<body>
-    <!-- HEADER -->
-    <?php
-        require_once('includes/header.php');
-    ?>
-    <!-- MAIN -->
-    <main>
+<!-- HEADER -->
+<?php
+    require_once('includes/header.php');
+?>
+<!-- MAIN -->
+<main>
+    <div class="index">
         <div class="logo">
-            <div class="logo-img">
-                <img src="includes/logo.jpg" alt="logo">
+            <div class="logo-content">
+                <div class="logo-front">
+                    <img src="includes/logo.jpg" alt="logo">
+                </div>
+                <div class="logo-back">
+                    <div class="socials">
+                        <div class="social insta">
+                            <i class="fab fa-instagram" style></i>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
         </div>
-    </main>
+    </div>
+</main>
 
-    <!-- FOOTER -->
-</body>
-</html>
+<!-- FOOTER -->
